2df30a27fb0b79d17aeb561e84c35d04302f34fa
Rollinon <1042920206@qq.com>
2022-07-20 15:37:43 +0800

    完成oh-my-zsh简易脚本

--- run.sh
+++
@@ -1,6 +0,0 @@
-host_ip=$(cat /etc/resolv.conf |grep "nameserver" |cut -f 2 -d " ")
-export ALL_PROXY="http://$host_ip:7890"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-sudo cp ./zshrc.bak ~/.zshrc
-zsh
